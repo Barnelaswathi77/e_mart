c740a91ff724acb379695a0f99dde0b13f2bb0aa
swathi <swathi21@navgurukul.org>
2024-08-17 12:49:59 +0530

    Navbar

---
+++ src/stores/components/Products.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Mobiles from './Mobiles'
+import Computers from './Computers'
+import Kitchen from './Kitchen'
+import Ac from './Ac'
+import Fridge from './Fridge'
+import Furniture from './Furniture'
+import Men from './Men'
+import Watch from './Watch'
+import Woman from './Woman'
+
+const Products = () =>{
+    return(
+        <>
+            <Mobiles />
+            <Computers />
+            <Kitchen/>
+            <Ac/>
+            <Fridge />
+            <Furniture/>
+            <Men/>
+            <Watch />
+            <Woman />
+            
+        </>
+    )
+}
+export default Products
---
+++ src/stores/pages/Landingpage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Products from '../components/Products'
+
+
+const Landindpage = () => {
+    return(
+        <> 
+            <Navbar />
+            <Products />
+        </>
+    )
+}
+export default Landindpage
